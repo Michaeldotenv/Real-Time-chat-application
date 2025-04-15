833b1d099fbafac953375acf248ee0d911ecb0e1
Tubokeyi Micheal <mtubokeyi@gmail.com>
2025-04-15 20:47:25 +0100

    my commit

--- Backend/src/middleware/protectUpdateRoute.js
+++ Backend/src/middleware/protectUpdateRoute.js
@@ -1,6 +1,6 @@
 
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/usermodel.js';
 
 const protectUpdateRoute = async (req, res, next) => {
   try {
